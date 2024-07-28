664b48ba1cd8edc12af954d4b22f6e564798f9cf
Nguyễn Mạnh <manhnguyenls131@gmail.com>
2024-07-29 03:24:55 +0700

    Uploaded | About, Careers, Security pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 let dragBtn = document.querySelector("#dragBtn")
 let mobileMenu = document.querySelector("#menu-mobile")
+let path = location.pathname;
 
 dragBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden")
 })
 
 
+console.log(path);
